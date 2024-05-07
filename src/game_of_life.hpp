202b86ad6c26a420885d2472854e2bee37c0c69b
lyes-hamici <lyes.hamici@laplateforme.io>
2024-05-07 14:07:03 +0200

    <feat>[game_of_life.cpp][game_of_life.hpp]<Change of the random method for placing dead or alive cells and reduction of the screen size, increase in the size of cells.>

--- src/game_of_life.hpp
+++ src/game_of_life.hpp
@@ -1,9 +1,9 @@
 #ifndef GAME_OF_LIFE_H
 #define GAME_OF_LIFE_H
 
-const int rows = 150;
-const int columns = 150;
-const int cell_size = 5;
+const int rows = 80;
+const int columns = 80;
+const int cell_size = 10;
 
 void initializeGame();
 void updateGame();
