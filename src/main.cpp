68b035e73bfc26caa2fff544f91667d87b8a8b1e
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-05-10 15:28:47 +0200

    <feat>[main.cpp]:<added fonctions play, stop, next frame, load random grid and clear the grid>

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,20 @@
 #include "game_of_life.hpp"
 #include <iostream>
 
-const int screenWidth = (columns) * cell_size;
-const int screenHeight = (rows) * cell_size + BottomMargin;
+const int screenWidth = (columns)*cell_size;
+const int screenHeight = (rows)*cell_size + BottomMargin;
 
-// Bouton gauche
+// Buttons on the left {play, stop and next frame}
 Rectangle leftButtons[3] = {
     {10, screenHeight - 50, 80, 40},
     {100, screenHeight - 50, 80, 40},
-    {190, screenHeight - 50, 80, 40}
-};
+    {190, screenHeight - 50, 80, 40}};
 
-// Bouton droit
+// Boutons on the right {random, pattern et clear }
 Rectangle rightButtons[3] = {
     {screenWidth - 270, screenHeight - 50, 80, 40},
     {screenWidth - 180, screenHeight - 50, 80, 40},
-    {screenWidth - 90, screenHeight - 50, 80, 40}
-};
+    {screenWidth - 90, screenHeight - 50, 80, 40}};
 
 bool isPlay = false;
 int frameCount = -1;
@@ -26,7 +24,6 @@ int main()
 {
     InitWindow(screenWidth, screenHeight, "Game of Life - ORTHLY");
     SetTargetFPS(60);
-    // initializeRandomGame();
     while (!WindowShouldClose())
     {
         if (isPlay || frameCount == 0)
@@ -35,30 +32,37 @@ int main()
             frameCount = -1;
         }
 
-        // Vérifier l'interaction avec les boutons
+        // It verifies if the user click on a button
         for (int i = 0; i < 3; ++i)
         {
             if (CheckCollisionPointRec(GetMousePosition(), leftButtons[i]) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
             {
-                if (i == 0) {
-                    std::cout << "test" << std::endl;
-                    // Action du bouton "play"
-                } else if (i == 1) {
-                    std::cout << "test1" << std::endl;
-                    // Action du bouton "stop"
-                } else if (i == 2) {
-                    // Action du bouton "next"
+                if (i == 0) //Play
+                {
+                    isPlay = true;
+                }
+                else if (i == 1) //Stop
+                {
+                    isPlay = false;
+                }
+                else if (i == 2) //Next
+                {
+                    frameCount = 0;
                 }
             }
 
             if (CheckCollisionPointRec(GetMousePosition(), rightButtons[i]) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
             {
-                if (i == 0) {
-                    // Action du bouton "random"
-                } else if (i == 1) {
-                    // Action du bouton "clear"
-                } else if (i == 2) {
-                    // Action du bouton "pattern"
+                if (i == 0) //Random
+                {
+                    initializeRandomGame();
+                }
+                else if (i == 1) // Pattern load
+                {
+                }
+                else if (i == 2) // Clear grid
+                {
+                    resetGrid();
                 }
             }
         }
@@ -67,13 +71,13 @@ int main()
         ClearBackground(RAYWHITE);
         drawGame();
 
-        // Dessiner les boutons
+        // Draw GUI: The buttons
         for (int i = 0; i < 3; ++i)
         {
             DrawRectangleRec(leftButtons[i], LIGHTGRAY);
             DrawRectangleRec(rightButtons[i], LIGHTGRAY);
             DrawText((i == 0) ? "play" : (i == 1) ? "stop" : "next", leftButtons[i].x + 10, leftButtons[i].y + 10, 20, DARKGRAY);
-            DrawText((i == 0) ? "random" : (i == 1) ? "clear" : "pattern", rightButtons[i].x + 10, rightButtons[i].y + 10, 20, DARKGRAY);
+            DrawText((i == 0) ? "random" : (i == 1) ? "pattern" : "clear", rightButtons[i].x + 10, rightButtons[i].y + 10, 20, DARKGRAY);
         }
 
         EndDrawing();
