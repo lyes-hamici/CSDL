ded466fc27232418203c995f5dca9618b5622a1f
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-05-12 18:28:00 +0200

    <test><algorithme><game_of_life>:<adding new algo files and adding them into game_of_life.cpp in order to be used in updateGame function>

--- src/algorithme.cpp
+++ src/algorithme.cpp
@@ -1,10 +1,11 @@
+#include "algorithme.hpp"
+#include "game_of_life.hpp"
 
 #include <vector>
 #include <iostream>
 #include <string>
 
-
-void check_neihbourg(int x, int y, int rows, int columns, std::vector<std::vector<int>>& grid) {
+void check_neihbourg(int x, int y, int rows, int columns, bool grid[80][80], bool new_grid[80][80]) {
     int count = 0;
     for (int i = -1; i < 2; i++) {
         for (int j = -1; j < 2; j++) {
@@ -18,28 +19,38 @@ void check_neihbourg(int x, int y, int rows, int columns, std::vector<std::vecto
     count -= grid[x][y];
     if (grid[x][y] == 1) {
         if (count < 2 || count > 3) {
-            grid[x][y] = 0;
+            new_grid[x][y] = 0;
         }
-    } else {
+        else {
+            new_grid[x][y] = 1;
+        }
+    }
+    else {
         if (count == 3) {
-            grid[x][y] = 1;
+            new_grid[x][y] = 1;
         }
     }
 }
 
-void updateGame(int rows, int columns, std::vector<std::vector<int>>& grid) {
-    std::vector<std::vector<int>> new_grid(rows, std::vector<int>(columns, 0));
+void updateMatrice(int rows, int columns, bool grid[80][80], bool new_grid[80][80]) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
-            check_neihbourg(i, j, rows, columns, grid);
+            check_neihbourg(i, j, rows, columns, grid, new_grid);
         }
     }
-}
-
-int main() {
-    std::vector<std::string> v = {"Hello", "World"};
-    for (const auto& s : v) {
-        std::cout << s << std::endl;
+    std::cout << "old grid: " << std::endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            std::cout << grid[i][j] << " ";
+        }
+        std::cout << std::endl;
     }
-    return 0;
+    grid = new_grid;
+    std::cout << "new grid: " << std::endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            std::cout << grid[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
